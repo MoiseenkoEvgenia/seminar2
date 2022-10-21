07a04a3f0c56aa5f791e2578220ddcc57af64209
MoiseenkoEvgenia <sokolova-2206@mail.ru>
2022-10-21 21:00:21 +0300

    add Exampie 001

---
+++ Exampie001/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу которая принимает число и показывает последнюю цифру этого числа
+int GetLastDigit(int number)
+{
+    return number % 10;
+}
+
+int testNumber = 5123890;
+
+Console.WriteLine(GetLastDigit(testNumber));
